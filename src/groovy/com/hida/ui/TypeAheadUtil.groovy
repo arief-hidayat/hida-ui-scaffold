0d4342d73e13896ffba8ec3b27e208f9e98530f6
ärief <mr.arief.hidayat@gmail.com>
2015-02-28 20:26:13 +0800

    Fix bug in typeAheadUtil

--- src/groovy/com/hida/ui/TypeAheadUtil.groovy
+++ src/groovy/com/hida/ui/TypeAheadUtil.groovy
@@ -5,12 +5,14 @@ package com.hida.ui
  */
 class TypeAheadUtil {
 
-    static void setupBasicAttrs(Map attrs, String domain, String displayKey,
-                                String domainIdField, String domainDisplayField = displayKey) {
+    static void setupBasicAttrs(Map attrs, String domain, String displayKey, // display key is field in original domain
+                                String domainIdField, String domainDisplayField = displayKey) { // domainDisplayField == field in parent
         attrs.items = "all"; attrs.minLength = "1";
         attrs.domain = domain; attrs.displayKey = displayKey;
 
-        attrs.parentInstance = attrs.for ?: "ta$domain"; attrs.field = attrs.field ?: domainDisplayField;
+        attrs.parentInstance = attrs.for ?: "ta$domain";
+        attrs.field = attrs.field ?: domainDisplayField;
+
         if(!attrs.populatedFieldsConf) attrs.populatedFieldsConf = [:]
         if(attrs.populatedFieldsConf.isEmpty()) {
             if(domainIdField) attrs.populatedFieldsConf.put(domainIdField, 'id')
@@ -20,18 +22,16 @@ class TypeAheadUtil {
             attrs.value = attrs.value ?: attrs.parentValue["${attrs.field}"]
             if(!attrs.fields && !(attrs.populatedFieldsConf).isEmpty()) {
                 def fieldData = [:]
-                attrs.populatedFieldsConf.each { String fieldNm, v ->
-                    fieldData[v] = attrs.parentValue[fieldNm]
+                attrs.populatedFieldsConf.each { String parentFieldNm, originalFieldNm ->
+                    fieldData[parentFieldNm] = attrs.parentValue[parentFieldNm] // changed
                 }
                 attrs.fields = fieldData
             }
         } else {
             if(!attrs.fields && !(attrs.populatedFieldsConf).isEmpty()) {
-                if(domainIdField) {
-                    attrs.fields = [:]
-                    attrs.populatedFieldsConf.each { String fieldNm, originalFieldNm ->
-                        attrs.fields.put(originalFieldNm, fieldNm == displayKey ? attrs.value : null )
-                    }
+                attrs.fields = [:]
+                attrs.populatedFieldsConf.each { String parentFieldNm, originalFieldNm ->
+                    attrs.fields.put(parentFieldNm, originalFieldNm == displayKey ? attrs.value : '' )
                 }
             }
         }
