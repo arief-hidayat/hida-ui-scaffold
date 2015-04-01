a67a5fe40bec32a46aab0d41c4b088cb4ef8eef7
ärief <mr.arief.hidayat@gmail.com>
2015-04-01 08:05:23 +0800

    continue on new view

--- grails-app/assets/javascripts/app/ui-components/app-hida-custom-table-formv2.js
+++ grails-app/assets/javascripts/app/ui-components/app-hida-custom-table-formv2.js
@@ -31,33 +31,32 @@
 
     App.view.SearchTableFormPage = App.View.extend({
         el : '#content-section',
-        tableEl : '#list-section', searchEl : "#search-section", detailEl : '#detail-section', messageEl : ".message-container",
+        tableEl : '#list-section', searchEl : "#search-section", detailEl : '#detail-section', messageEl : "#message-section",
         tablePubSub : null, searchPubSub: null, detailPubSub : null,
         tableView : null, searchView : null, detailView : null, $message : null,
         tableConfig : null, searchConfig : null, formConfig : null,
+        actionConfig : null,
         events : { },
         initialize: function(opt) {
             this.tableEl = opt.tableEl || this.tableEl; this.detailEl = opt.detailEl || this.detailEl; this.searchEl = opt.searchEl || this.searchEl;
             this.tablePubSub = _.extend({},Backbone.Events); this.searchPubSub = _.extend({},Backbone.Events); this.detailPubSub = _.extend({},Backbone.Events);
-
             this.tableConfig = opt.tableConfig; this.searchConfig = opt.searchConfig; this.formConfig = opt.formConfig;
 
-            this.setupInitView(opt);
+            var urlController = this.key.charAt(0).toLowerCase() + this.key.substr(1);
+            this.actionConfig = opt.actionConfig || {
+                show : { url : (App.url + "/" + urlController + "/showForm/"), formInit : "setReadOnlyForm" , params : ["id"]},
+                create : { url : (App.url + "/" + urlController + "/createForm/"), formInit : "setEditableForm", params : [] },
+                edit : { url : (App.url + "/" + urlController + "/editForm/"), formInit : "setEditableForm" , params : ["id"]}
+            };
+            this.$message = this.$(this.messageEl);
+            //TODO: build search form.
+
+            this.buildTable();
+            this.initTableActionListeners(opt);
+            this.initFormActionListeners(opt);
 
             App.AJAX.errorHtmlCall = App.AJAX.errorHtmlCall || App.AJAX.defaultErrorCallback(this.$message);
             App.AJAX.errorJsonCall = App.AJAX.errorJsonCall || App.AJAX.defaultErrorCallback(this.$message);
-        },
-        remove: function() {
-            this.removeForm().removeTable();
-            return App.View.prototype.remove.apply(this, arguments);
-        },
-        removeForm : function() {
-            if(this.detailView != null) { this.detailView.remove(); this.detailView = undefined; }
-            return this;
-        },
-        removeTable: function() {
-            if(this.tableView != null) { this.tableView.remove(); this.tableView = undefined; }
-            return this;
         },
         initTableActionListeners : function(opt) {
             var urlController = this.key.charAt(0).toLowerCase() + this.key.substr(1);
@@ -85,9 +84,8 @@
                 }
             }
         },
-        loadForm : function(url, action) {
+        loadForm : function(url, action, buildFormParam) { // buildFormParam : action & id
             return function(eventData) {
-//                App.logDebug("enter loadForm from url" + url);
                 if(!action) {
                     if(url == this.urlCreateForm) action = "create";
                     else if(url == this.urlShowForm) action = "show";
@@ -106,9 +104,9 @@
                 var callback = (
                     function(buildFormParam) {
                         return function( data ) {
-                            this.removeForm();
+                            this.removeDetailForm();
                             this.$(this.detailEl).html(data); // note that 'append' only work for two tabs.
-                            this.buildForm(buildFormParam);
+                            this.buildDetailForm(buildFormParam);
                             this.showForm();
                         }
                     }
@@ -125,19 +123,18 @@
             }
             return opt;
         },
-        getStringId : function(formId) {
-            return formId.id;
-        },
         buildTable : function(publishReload) {
-            if(this.tableView == undefined) {
-                this.tableView = new App.view.TableTabRegion( {el: this.tableEl, key: this.key,
-                    pubSub: this.tablePubSub, customUrl : this.tableConfig} ); // TODO: change to normal Table.
-            }
-            else this.tableView.reloadTable();
+            if(this.tableView == undefined) this.initTable(); else this.reloadTable();
             if(publishReload) this.publishTableEvt("table:reloaded", {});
         },
-        buildForm : function(initialForm) {
-            if(this.formView == undefined) {
+        // Table-override start
+        initTable : function() {
+            this.tableView = new App.view.TableTabRegion( {el: this.tableEl, key: this.key, pubSub: this.tablePubSub, customUrl : this.tableConfig} );
+        },
+        reloadTable : function() { this.tableView.reloadTable(); },
+        // Table-override end
+        buildDetailForm : function(initialForm) {
+            if(this.detailView == undefined) {
                 var options = {el: this.detailEl, key: this.key, pubSub: this.detailPubSub};
                 if(initialForm) {
                     if(initialForm.id) options.formId = initialForm.id;
@@ -150,37 +147,19 @@
                 this.setEditableForm(options);
             }
         },
+        // detail form override -start
         setReadOnlyForm : function(options) {
-//            this.form = new App.view.ReadOnlyForm(options);
+            this.detailView = new App.view.CombinedForm(options);
         },
         setEditableForm : function(options) {
-//            this.form = new App.view.EditableForm(options);
+            this.detailView = new App.view.CombinedForm(options);
         },
-        resetForm : function() {
-            this.getHtml(this.urlCreateForm, null, function( data ) {
-                this.removeForm();
-                $(this.detailEl).html(data); // note that 'append' only work for two tabs.
-                this.buildForm();
-            });
-        },
-        setupInitView : function(opt) {
-            this.$message = this.$(this.messageEl);
-            //TODO: build search form.
-
-            this.buildTable();
-            this.initTableActionListeners(opt);
-//            if(opt.initialForm && opt.initialForm.id) {
-//                this.buildForm(opt.initialForm);
-//            }
-            this.initFormActionListeners(opt);
-        },
+        // detail form override -start
         getFormId : function() {
             var $formContainer = $(this.detailEl);
             if($formContainer) {
                 var $idField = $formContainer.find("[name='id']");
-                if($idField) {
-                    return {'id' : $idField.val()};
-                }
+                if($idField) return {'id' : $idField.val()};
             }
             return null;
         },
@@ -211,6 +190,9 @@
             var url = dt.url, form = dt.form;
             this.ajaxRequestForPartialView("POST", url, form , { id : form.id, action : "show"}, { id : form.id, action : "update"}, true);
         },
+        onPartialViewRequest : function() {
+
+        },
         getHtml : function(url, option, callback) {
             return App.AJAX.getHTML(this, url, option, callback);
         },
@@ -226,17 +208,33 @@
         ajaxRequestForPartialView : function(action, url, form, expectedForm, initialForm, publishReloadEvt) {
             var successCallback = (function(expectedForm, publishReloadEvt) {
                 return function( data ) {
-                    this.removeForm();
+                    this.removeDetailForm();
                     $(this.detailEl).html(data); // note that 'append' only work for two tabs.
                     if(expectedForm.action == "show" && !expectedForm.id) {
                         expectedForm.id = $(this.detailEl).find("[name='id']").val();
                     }
-                    this.buildForm(expectedForm); // pass id, action
+                    this.buildDetailForm(expectedForm); // pass id, action
                     this.buildTable(publishReloadEvt);
                 }
             })(expectedForm, publishReloadEvt);
 //            form._partial = true; //TODO: remove _partial logic in controller
             this.ajaxHtml(action, url, form, successCallback);
+        },
+        remove: function() {
+            this.removeDetailForm().removeTable().removeSearchForm();
+            return App.View.prototype.remove.apply(this, arguments);
+        },
+        removeDetailForm : function() {
+            if(this.detailView != null) { this.detailView.remove(); this.detailView = undefined; }
+            return this;
+        },
+        removeSearchForm : function() {
+            if(this.searchView != null) { this.searchView.remove(); this.searchView = undefined; }
+            return this;
+        },
+        removeTable: function() {
+            if(this.tableView != null) { this.tableView.remove(); this.tableView = undefined; }
+            return this;
         },
         publishTableEvt : function(code, data) { this.tablePubSub.trigger(code, _.extend({key : this.key}, data)); },
         subscribeTableEvt : function(code, callback) { this.listenTo(this.tablePubSub, code, callback ,this); },
